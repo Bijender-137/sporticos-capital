528a215a3d79ae89ca8ae731eda36642b7ae85e3
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-16 15:41:37 +0530

    navside

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import { MenuIcons } from "./common/Icons";
+import mainLogo from "../assets/images/webp/main-logo.webp";
+
 const Header = () => {
   const [show, setShow] = useState(false);
   return (
     <>
-      <div className={`${show ? "left_true" : "left_300"} navbars`}>
-        <ul className='py-5 text-center text-center'>
-          <li>home</li>
-          <li className='py-5'>About</li>
-          <li>Services</li>
-          <button
-            onClick={() => setShow(false)}
-            className="btn btn-danger px-4 mt-5 py-2">
-            close
-          </button>
-        </ul>
-      </div>
-      <button
-        onClick={() => setShow(true)}
-        className="btn btn-danger px-4 mt-5 py-2">
-        Open
-      </button>
+      <nav className="bg-yellow">
+        <Container>
+          <div className={`${show ? "left_true" : "left_300"} navbars bg-white`}>
+            <ul className='p-4 mb-0'>
+              <li>home</li>
+              <li className='py-5'>About</li>
+              <li>Services</li>
+              <button
+                onClick={() => setShow(false)}
+                className="btn btn-danger px-4 mt-5 py-2">
+                close
+              </button>
+            </ul>
+          </div>
+          <div className="d-flex justify-content-between align-items-center py-3">
+            <img loading="lazy" width={261} height={49} src={mainLogo} alt="mainlogo-page" />
+            <div onClick={() => setShow(true)} className="cursor_pointer">
+              <MenuIcons />
+            </div>
+          </div>
+        </Container>
+      </nav>
 
-     
     </>
   )
 }
