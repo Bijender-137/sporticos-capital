2de32cd8f2f4051322f87279efb26cb4f420bde0
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-16 15:10:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return <>
+    <Header />
+  </>;
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Header = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, vitae.
-    </div>
-  );
-};
+    <>
+      <div className={`${show ? "left_true" : "left_300"} navbars`}>
+        <ul className='py-5 text-center text-center'>
+          <li>home</li>
+          <li className='py-5'>About</li>
+          <li>Services</li>
+          <button
+            onClick={() => setShow(false)}
+            className="btn btn-danger px-4 mt-5 py-2">
+            close
+          </button>
+        </ul>
+      </div>
+      <button
+        onClick={() => setShow(true)}
+        className="btn btn-danger px-4 mt-5 py-2">
+        Open
+      </button>
+
+     
+    </>
+  )
+}
 
 export default Header;
