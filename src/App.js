1734d87482ea01676c36b3f144c7edaacf1804f2
Bijender-137 <bijender.k1996@gmail.com>
2023-11-16 15:01:16 +0530

    code pull

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+import FormSubmission from "./components/FormSubmission";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <FormSubmission />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/FormSubmission.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const FormSubmission = () => {
+  return (
+    <>
+      <div className="bg-white">
+        <Container>
+          <div>
+            <h2>
+              Submit your Information and a Member of our Team will Follow Up As
+              Soon As Possible
+            </h2>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default FormSubmission;
