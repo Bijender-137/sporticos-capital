4f400a94d05a1603f7b34d9de5b2b2c502254a6f
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-17 14:20:40 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <>
             <h1 className="fs-lg ff-fontspring text-white lh-lg-84 z-2 position-relative lh-md-60 mb-0 text-center text-lg-start pt-5 mt-4 mt-sm-0 pt-lg-0">Talk to The <span className='d-block clr-yellow'>Team</span></h1>
             <div className="d-flex justify-content-center justify-content-lg-start position-relative z-2">
-                <button className="mt-4 mt-sm-3 mt-lg-4 heroBtn fw-semibold text-uppercase text-black fs-xs bg-yellow navLink-button ffBarlow" type="button">
+                <button className="mt-4 mt-sm-3 mt-lg-4 heroBtn fw-semibold text-uppercase text-black fs-xs bg-yellow navLink-button ff-fontspring" type="button">
                     set an appointment
                 </button>
             </div>
