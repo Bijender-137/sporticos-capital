f3b0520c23d0f3079d06ff49a458eadc157088a0
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-17 09:56:41 +0530

    code merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { CrossIcons, MenuIcons } from "./common/Icons";
 import mainLogo from "../assets/images/webp/main-logo.webp";
 import { Link } from "react-router-dom";
+import Hero from "./Hero";
 
 const Header = () => {
   const [show, setShow] = useState(false);
@@ -21,13 +22,15 @@ const Header = () => {
                 <div onClick={() => setShow(false)} className="position-absolute crossIcon">
                   <CrossIcons />
                 </div>
-                <Link className="text-black mb-4 ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>Club Investments</Link>
-                <Link className="text-black mb-4 ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>Academy Partnerships</Link>
-                <Link className="text-black mb-4 ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>Scouting and Trials</Link>
-                <Link className="text-black mb-4 ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>E-Mentoring</Link>
-                <Link className="text-black mb-4 ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>Venture Capital</Link>
-                <Link className="text-black mb-4 ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>CSR</Link>
-                <Link className="text-black ff-barlow fw-normal fs-xs opacity-07" onClick={() => setShow(false)}>About</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>Club Investments</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>Academy Partnerships</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>Scouting and Trials</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>E-Mentoring</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>Venture Capital</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>CSR</Link>
+                <Link className="text-black mb-3 mb-md-4 ffBarlow fw-normal fs-xs opacity-07 navlink" onClick={() => setShow(false)}>About</Link>
+                <button className="px-3 py-2 fw-semibold text-uppercase rounded-1 text-black fs-xs bg-yellow navLink-button ffBarlow mb-3 mb-md-4" type="button" onClick={() => setShow(false)}>contant Us</button>
+                <button className="px-4 py-2 fw-semibold text-uppercase rounded-1 text-black fs-xs bg-yellow navLink-button ffBarlow" type="button" onClick={() => setShow(false)}>set an appointment</button>
               </div>
             </div>
             <div className="d-flex justify-content-between align-items-center py-3">
@@ -40,10 +43,7 @@ const Header = () => {
         </div>
         <div className=" flex-grow-1 d-flex align-items-center">
           <Container>
-            <h1 className="fs-lg">Talk to The
-              <span className="d-sm-block">
-                Team
-              </span></h1>
+            <Hero />
           </Container>
         </div>
       </nav>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <>
+            <h1 className="fs-lg">Talk to The
+                <span className="d-sm-block">
+                    Team
+                </span>
+            </h1>
+        </>
+    )
+}
+
+export default Hero
