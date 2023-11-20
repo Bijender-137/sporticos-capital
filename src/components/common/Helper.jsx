899eed7fbd39cd834f6fad76b8117ce473c683ba
Bijender-137 <bijender.k1996@gmail.com>
2023-11-20 09:58:00 +0530

    changes

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,81 +1,89 @@
 export const accordionItems = [
-    {
-        id: 1,
-        title: 'What is the primary focus of your club investments?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 2,
-        title: 'How do you minimize risks for sports investors?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 3,
-        title: 'How does your e-mentoring platform work?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 4,
-        title: 'What is your philosophy in terms of wealth management?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 5,
-        title: 'Which investments are included as part of your wealth management offering?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 6,
-        title: 'Who can benefit from your wealth management solutions?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 7,
-        title: 'What is the objective of your player pathways program?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-    {
-        id: 8,
-        title: 'How do your debt funding solutions work?',
-        content: 'We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.'
-    },
-]
+  {
+    id: 1,
+    title: "What is the primary focus of your club investments?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 2,
+    title: "How do you minimize risks for sports investors?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 3,
+    title: "How does your e-mentoring platform work?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 4,
+    title: "What is your philosophy in terms of wealth management?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 5,
+    title:
+      "Which investments are included as part of your wealth management offering?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 6,
+    title: "Who can benefit from your wealth management solutions?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 7,
+    title: "What is the objective of your player pathways program?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+  {
+    id: 8,
+    title: "How do your debt funding solutions work?",
+    content:
+      "We invest in high-potential football clubs, leagues, and adjacent real estate with meaningful upside potential for investors. Fill out the form on our Club Investments page or book an appointment to learn more.",
+  },
+];
 
 export const navLinks = [
-    {
-        id: 1,
-        NavName: "Club Investments",
-        pathName: "/",
-    },
-    {
-        id: 2,
-        NavName: "Academy Partnerships",
-        pathName: "/academy",
-    },
-    {
-        id: 3,
-        NavName: "Scouting and Trials",
-        pathName: "/scouting",
-    },
-    {
-        id: 4,
-        NavName: "E-Mentoring",
-        pathName: "/mentoring",
-    },
-    {
-        id: 5,
-        NavName: "Venture Capital",
-        pathName: "/venture",
-    },
-    {
-        id: 6,
-        NavName: "CSR",
-        pathName: "/csr",
-    },
-    {
-        id: 7,
-        NavName: "About",
-        pathName: "/about",
-    },
-
-]
+  {
+    id: 1,
+    NavName: "Club Investments",
+    pathName: "/",
+  },
+  {
+    id: 2,
+    NavName: "Academy Partnerships",
+    pathName: "/academy",
+  },
+  {
+    id: 3,
+    NavName: "Scouting and Trials",
+    pathName: "/scouting",
+  },
+  {
+    id: 4,
+    NavName: "E-Mentoring",
+    pathName: "/mentoring",
+  },
+  {
+    id: 5,
+    NavName: "Venture Capital",
+    pathName: "/venture",
+  },
+  {
+    id: 6,
+    NavName: "CSR",
+    pathName: "/csr",
+  },
+  {
+    id: 7,
+    NavName: "About",
+    pathName: "/about",
+  },
+];
