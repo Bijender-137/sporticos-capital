9de00d645c24bd9d71891e0ad242c23b069a4f98
Bijender-137 <bijender.k1996@gmail.com>
2023-11-20 17:03:57 +0530

    code change

--- src/components/Dreams.jsx
+++ src/components/Dreams.jsx
@@ -9,7 +9,7 @@ const Dreams = () => {
     <>
       <div className="py-5">
         <Container className="py-lg-5">
-          <div className="pb-sm-3">
+          <div className="pb-sm-3 pt-md-3">
             {dreams.map((dreamsplayer) => {
               const dreamplayerchange =
                 dreamsplayer.id === dreamsplayer.id &&
@@ -25,23 +25,47 @@ const Dreams = () => {
                   <Row className="pt-3 pt-sm-4 pt-lg-5 justify-content-center">
                     <Col sm={6} lg={4}>
                       <div className="dreamPlayer-shadow p-3 h-100">
-                        <img className="w-100" src={dreamsplayer.dremImg} alt="dream img" />
-                        <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">{dreamsplayer.subHead}</h2>
-                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2">{dreamsplayer.paragraph}</p>
+                        <img
+                          className="w-100 overflow-hidden"
+                          src={dreamsplayer.dremImg}
+                          alt="dream img"
+                        />
+                        <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">
+                          {dreamsplayer.subHead}
+                        </h2>
+                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3">
+                          {dreamsplayer.paragraph}
+                        </p>
                       </div>
                     </Col>
                     <Col sm={6} lg={4} className="pt-3 pt-sm-0">
                       <div className="dreamPlayer-shadow p-3 h-100 ">
-                        <img className="w-100" src={dreamsplayer.dremImg2} alt="dream img" />
-                        <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">{dreamsplayer.subHead2}</h2>
-                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2">{dreamsplayer.paragraph2}</p>
+                        <img
+                          className="w-100 overflow-hidden"
+                          src={dreamsplayer.dremImg2}
+                          alt="dream img"
+                        />
+                        <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">
+                          {dreamsplayer.subHead2}
+                        </h2>
+                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 me-xl-1">
+                          {dreamsplayer.paragraph2}
+                        </p>
                       </div>
                     </Col>
                     <Col sm={6} lg={4} className="pt-3 pt-lg-0">
-                      <div className="dreamPlayer-shadow p-3 h-100 ">
-                        <img className="w-100" src={dreamsplayer.dremImg3} alt="dream img" />
-                        <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">{dreamsplayer.subHead3}</h2>
-                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2">{dreamsplayer.paragraph3}</p>
+                      <div className="dreamPlayer-shadow p-3 h-100 transition_300">
+                        <img
+                          className="w-100 overflow-hidden"
+                          src={dreamsplayer.dremImg3}
+                          alt="dream img"
+                        />
+                        <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">
+                          {dreamsplayer.subHead3}
+                        </h2>
+                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 me-xl-1">
+                          {dreamsplayer.paragraph3}
+                        </p>
                       </div>
                     </Col>
                   </Row>
