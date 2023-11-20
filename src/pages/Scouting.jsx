9c5849e29c8d894ca41b5f7875b4f9539ab33114
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-20 13:49:00 +0530

    code

--- src/pages/Scouting.jsx
+++ src/pages/Scouting.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FormSubmission from "../components/FormSubmission";
 import Dreams from "../components/Dreams";
+import Nextlevel from "../components/Nextlevel";
 
 const Scouting = () => {
   return (
     <div>
       <Dreams />
+      <Nextlevel />
       <FormSubmission />
     </div>
   );
