50b554b6d3c8869e11d2a1d24ad7cee99d3e8abf
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-20 14:42:37 +0530

    changes

--- src/components/Nextlevel.jsx
+++ src/components/Nextlevel.jsx
@@ -13,24 +13,26 @@ const Nextlevel = () => {
 
     return (
         <>
-            <Container>
-                {nextlevelData.map((nextlevel, index) => (
-                    <div key={index}>
-                        <h5 className='fs-xxl fw-semibold ff-fontspring text-center text-black mb-1'>{nextlevel.heading}</h5>
-                        <p className='fs-base ffBarlow opacity-07 text-black fw-normal text-center'>{nextlevel.para}<span className='d-lg-block'>{nextlevel.spanpara}</span></p>
-                        <Row className='d-flex justify-content-center pt-lg-3'>
-                            {nextlevel.submap.map((levelsub, columnindex) => (
-                                <Col key={columnindex} lg={3} md={6} className='p-sm-0 pt-3 pt-sm-3 pt-lg-0'>
-                                    <div className={`nextlevel-box ${columnindex === nextlevel.submap.length - 1 ? 'last-child' : ''}`}>
-                                        <div className="d-flex justify-content-center justify-content-md-start">{levelsub.icon}</div>
-                                        <p className='ffBarlow fs-base fw-normal text-black mb-0 pt-4 mt-2 text-center text-md-start'>{levelsub.paraBox}</p>
-                                    </div>
-                                </Col>
-                            ))}
-                        </Row>
-                    </div>
-                ))}
-            </Container>
+            <div className="py-5">
+                <Container>
+                    {nextlevelData.map((nextlevel, index) => (
+                        <div key={index}>
+                            <h5 className='fs-xxl fw-semibold ff-fontspring text-center text-black mb-1'>{nextlevel.heading}</h5>
+                            <p className='fs-base ffBarlow opacity-07 text-black fw-normal text-center'>{nextlevel.para}<span className='d-lg-block'>{nextlevel.spanpara}</span></p>
+                            <Row className='d-flex justify-content-center pt-lg-3'>
+                                {nextlevel.submap.map((levelsub, columnindex) => (
+                                    <Col key={columnindex} lg={3} md={6} className='p-sm-0 pt-3 pt-sm-3 pt-lg-0'>
+                                        <div className={`nextlevel-box ${columnindex === nextlevel.submap.length - 1 ? 'last-child' : ''}`}>
+                                            <div className="d-flex justify-content-center justify-content-md-start">{levelsub.icon}</div>
+                                            <p className='ffBarlow fs-base fw-normal text-black mb-0 pt-4 mt-2 text-center text-md-start'>{levelsub.paraBox}</p>
+                                        </div>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    ))}
+                </Container>
+            </div>
         </>
     );
 };
