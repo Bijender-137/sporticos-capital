94e7571f8fcad53f9808bc220311ddeb3a9e59ea
Bijender-137 <bijender.k1996@gmail.com>
2023-11-21 10:00:03 +0530

    updates

--- src/components/Dreams.jsx
+++ src/components/Dreams.jsx
@@ -9,7 +9,7 @@ const Dreams = () => {
     <>
       <div className="py-5">
         <Container className="py-lg-5">
-          <div className="pb-sm-3 pt-md-3">
+          <div className="py-md-3">
             {dreams.map((dreamsplayer) => {
               const dreamplayerchange =
                 dreamsplayer.id === dreamsplayer.id &&
@@ -19,21 +19,21 @@ const Dreams = () => {
                   <p className="ff-fontspring fw-normal fs-xxl lh-42 text-center text-capitalize mb-0">
                     {dreamsplayer.heading}
                   </p>
-                  <p className="ffBarlow opacity-07 fw-normal fs-base text-black text-center mb-0 pt-1 m-auto">
+                  <p className="ffBarlow opacity-07 fw-normal fs-base text-black text-center mb-0 pt-1 m-auto max-w-500">
                     {dreamsplayer.para}
                   </p>
                   <Row className="pt-3 pt-sm-4 pt-lg-5 justify-content-center">
                     <Col sm={6} lg={4}>
                       <div className="dreamPlayer-shadow p-3 h-100">
                         <img
-                          className="w-100 overflow-hidden"
+                          className="w-100 overflow-hidden transition_300"
                           src={dreamsplayer.dremImg}
                           alt="dream img"
                         />
                         <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">
                           {dreamsplayer.subHead}
                         </h2>
-                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3">
+                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 mb-0">
                           {dreamsplayer.paragraph}
                         </p>
                       </div>
@@ -41,14 +41,14 @@ const Dreams = () => {
                     <Col sm={6} lg={4} className="pt-3 pt-sm-0">
                       <div className="dreamPlayer-shadow p-3 h-100 ">
                         <img
-                          className="w-100 overflow-hidden"
+                          className="w-100 overflow-hidden transition_300"
                           src={dreamsplayer.dremImg2}
                           alt="dream img"
                         />
                         <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">
                           {dreamsplayer.subHead2}
                         </h2>
-                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 me-xl-1">
+                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 me-xl-1 mb-0">
                           {dreamsplayer.paragraph2}
                         </p>
                       </div>
@@ -56,14 +56,14 @@ const Dreams = () => {
                     <Col sm={6} lg={4} className="pt-3 pt-lg-0">
                       <div className="dreamPlayer-shadow p-3 h-100 transition_300">
                         <img
-                          className="w-100 overflow-hidden"
+                          className="w-100 overflow-hidden transition_300"
                           src={dreamsplayer.dremImg3}
                           alt="dream img"
                         />
                         <h2 className="fw-sembild text-black fs-base ff-fontspring pt-4 mt-1">
                           {dreamsplayer.subHead3}
                         </h2>
-                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 me-xl-1">
+                        <p className="fw-normal ffBarlow fs-base text-black opacity-07 pt-2 pe-xl-5 pe-lg-3 me-xl-1 mb-0">
                           {dreamsplayer.paragraph3}
                         </p>
                       </div>
