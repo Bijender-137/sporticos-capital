a78c2312d94a037cf66dedf4d5b6f9ac4e772e4b
Bijender-137 <bijender.k1996@gmail.com>
2023-11-23 17:00:08 +0530

    code update

--- src/components/common/CommonTeam.jsx
+++ src/components/common/CommonTeam.jsx
@@ -23,20 +23,57 @@ const CommonTeam = () => {
                       {teams.para}
                     </p>
                   )}
-                  <Row>
-                    <Col md={6}>
-                      <div>
+                </div>
+                <Row>
+                  <Col md={6}>
+                    <div className="common_team_card_shadow p-4 h-100 transition-300">
+                      <div className="d-flex  align-items-center">
                         <img
                           width={142}
                           height={142}
-                          className=""
+                          className="transition-300"
                           src={teams.manimg}
                           alt="founder img"
                         />
+                        <div className="text-start ps-4 ms-2">
+                          <h3 className="ff-fontspring fs-lg clr_lite_green fw-normal">
+                            {teams.heading}
+                          </h3>
+                          <h3 className="ffBarlow fw-normal text-black opacity-07 fs-base pt-1">
+                            {teams.subheading}
+                          </h3>
+                        </div>
                       </div>
-                    </Col>
-                  </Row>
-                </div>
+                      <p className="ffBarlow fw-normal fs-base text-black opacity-07 lh-24 max_w_535 pt-4 pb-3">
+                        {teams.para}
+                      </p>
+                    </div>
+                  </Col>
+                  <Col md={6}>
+                    <div className="common_team_card_shadow p-4 h-100 transition-300">
+                      <div className="d-flex  align-items-center">
+                        <img
+                          width={142}
+                          height={142}
+                          className="transition-300"
+                          src={teams.girlImg}
+                          alt="founder img"
+                        />
+                        <div className="text-start ps-4 ms-2">
+                          <h3 className="ff-fontspring fs-lg clr_blue fw-normal">
+                            {teams.heading2}
+                          </h3>
+                          <h3 className="ffBarlow fw-normal text-black opacity-07 fs-base pt-1">
+                            {teams.subheading2}
+                          </h3>
+                        </div>
+                      </div>
+                      <p className="ffBarlow fw-normal fs-base text-black opacity-07 lh-24 max_w_535 pt-4 pb-3">
+                        {teams.para2}
+                      </p>
+                    </div>
+                  </Col>
+                </Row>
               </div>
             ) : null;
           })}
