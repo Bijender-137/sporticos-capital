23f7b35653c4aaa6bfe15a2f33cca647e9923e5e
Bijender-137 <bijender.k1996@gmail.com>
2023-11-23 11:03:31 +0530

    changes

--- src/components/common/Helper2.jsx
+++ src/components/common/Helper2.jsx
@@ -2,81 +2,44 @@ import PlayerFirst from "../../assets/images/webp/player-dreams-1.webp";
 import PlayerSecond from "../../assets/images/webp/player-dreams-2.webp";
 import PlayerThird from "../../assets/images/webp/player-dreams-3.webp";
 // form submission
-// export const formsubmit = [
-//   {
-//     id: 1,
-//     pathName: "/",
-//     heading:
-//       "Submit your Information and a Member of our  Team will Follow Up As Soon As Possible",
-//   },
-//   {
-//     id: 2,
-//     pathName: "/scouting",
-//     heading: "Unlock Each Player’s Full Potential",
-//     para: "To get in touch about our player pathways, we encourage you to submit the form below.",
-//   },
-//   {
-//     id: 3,
-//     pathName: "/scoutin",
-//     heading: "Leave Your Portfolio In Trusted Hands",
-//     para: "Take the first step toward our wealth management solutions by submitting the form below.",
-//   },
-//   {
-//     id: 4,
-//     pathName: "/scoutin",
-//     heading: "Discover the Advantage of E-Mentoring",
-//     para: "For more information about joining our e-mentoring program, simply fill out the form below.",
-//   },
-//   {
-//     id: 5,
-//     pathName: "/scoutin",
-//     heading: "Winning Investment Portfolios Start Here",
-//     para: "Fill out the form below to learn more about our club investment opportunities.",
-//   },
-//   {
-//     id: 6,
-//     pathName: "/scoutin",
-//     heading: "Secure Favorable Debt Funding Terms",
-//     para: "Submit the form below for more information about our debt funding solutions.",
-//   },
-// ];
+
 export const formdata = [
   {
     id: 1,
     heading:
       "Submit your Information and a Member of our Team will Follow Up As Soon As Possible",
-    pathName: "/",
+    path: "/",
   },
+
   {
     id: 2,
-    heading: "FAQ",
-    pathName: "/faq",
+    heading: "Unlock Each Player’s Full Potential",
+    para: "To get in touch about our player pathways, we encourage you to submit the form below.",
+    path: "/play-pathwa",
   },
   {
     id: 3,
-    heading: "Unlock Each Player’s Full Potential",
-    para: "To get in touch about our player pathways, we encourage you to submit the form below.",
-    pathName: "/scouting",
+    heading: "Leave Your Portfolio In Trusted Hands",
+    para: "Take the first step toward our wealth management solutions by submitting the form below.",
+    path: "/wealth-management",
   },
   {
     id: 4,
-    heading: "E-Mentoring",
-    pathName: "/mentoring",
+    heading: "Unlock Each Player’s Full Potential",
+    para: "To get in touch about our player pathways, we encourage you to submit the form below.",
+    path: "/play-pathwa",
   },
   {
     id: 5,
-    heading: "Venture Capital",
-    pathName: "/venture",
+    heading: "Unlock Each Player’s Full Potential",
+    para: "To get in touch about our player pathways, we encourage you to submit the form below.",
+    path: "/play-pathwa",
   },
   {
     id: 6,
-    heading: "CSR",
-    pathName: "/csr",
-  },
-  {
-    id: 7,
-    heading: "About",
-    pathName: "/about",
+    heading: "Unlock Each Player’s Full Potential",
+    para: "To get in touch about our player pathways, we encourage you to submit the form below.",
+    path: "/play-pathwa",
   },
 ];
 export const inputdata = [
@@ -86,15 +49,17 @@ export const inputdata = [
     name: "Name",
     subject: "Subject",
     apointment: "Set an Appointment",
-    pathName: "/",
+    path: "/",
   },
+
   {
     id: 2,
     email: "Email*",
     name: "Name",
     subject: "Subject",
-    apointment: "Set an Appointment",
-    pathName: "/faq",
+    apointment: "Player Pathway - Potential",
+
+    path: "/play-pathwa",
   },
   {
     id: 3,
@@ -103,34 +68,41 @@ export const inputdata = [
     subject: "Subject",
     apointment: "Player Pathway - Potential",
 
-    pathName: "/scouting",
+    path: "/play-pathwa",
   },
   {
     id: 4,
-    email: "E-Mentoring",
-    pathName: "/mentoring",
+    email: "Email*",
+    name: "Name",
+    subject: "Subject",
+    apointment: "Player Pathway - Potential",
+
+    path: "/play-pathwa",
   },
   {
     id: 5,
-    email: "Venture Capital",
-    pathName: "/venture",
+    email: "Email*",
+    name: "Name",
+    subject: "Subject",
+    apointment: "Player Pathway - Potential",
+
+    path: "/play-pathwa",
   },
   {
     id: 6,
-    input: "CSR",
-    pathName: "/csr",
-  },
-  {
-    id: 7,
-    input: "About",
-    pathName: "/about",
+    email: "Email*",
+    name: "Name",
+    subject: "Subject",
+    apointment: "Player Pathway - Potential",
+
+    path: "/play-pathwa",
   },
 ];
 // dreams player
 export const dreams = [
   {
     id: 1,
-    pathName: "/scouting",
+    path: "/play-pathwa",
     heading: "Bringing Player Dreams to Fruition",
     para: "Our player pathways achieve two key outcomes, including player development and overarching club success.",
     dremImg: PlayerFirst,
@@ -148,10 +120,20 @@ export const dreams = [
   },
   {
     id: 2,
-    pathName: "/scouting-2",
-  },
-  {
-    id: 3,
-    pathName: "/scouting-3",
+    path: "/wealth-management",
+    heading: "Bringing Player Dreams to Fruition",
+    para: "Our player pathways achieve two key outcomes, including player development and overarching club success.",
+    dremImg: PlayerFirst,
+    dremImg2: PlayerSecond,
+    dremImg3: PlayerThird,
+    subHead: "Club Synergies",
+    paragraph:
+      "Improving the ROI of our club investments by connecting budding players with opportunities for measurable growth.",
+    subHead2: "Enhanced Exposure",
+    paragraph2:
+      "Training players on media engagements while leveraging our marketing packages to improve public sentiment, scouting potential, and club branding.",
+    subHead3: "Player Ascensions",
+    paragraph3:
+      "Coordinating with our global network of partner agencies and football industry contacts to ascend players into elite leagues.",
   },
 ];
