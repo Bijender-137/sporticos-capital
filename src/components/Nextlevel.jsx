b978ab3f7efef5c00609f5ddcc631ebade853cd5
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-24 10:10:58 +0530

    updates

--- src/components/Nextlevel.jsx
+++ src/components/Nextlevel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { nextlevelData } from "./common/Helper";
 import { useLocation } from "react-router-dom";
@@ -8,42 +8,53 @@ const Nextlevel = () => {
   const currentPathname = location.pathname;
   const currentData = nextlevelData.find((e) => e.path === currentPathname);
 
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(location.pathname === '/about-us' || location.pathname === '/debt-funding');
+  }, [location.pathname]);
+
   return (
-    <Container>
-      <div>
-        <p className="fs-xxl fw-semibold ff-fontspring text-center text-black mb-1">
-          {currentData.heading}
-        </p>
-        <p className="fs-base ffBarlow opacity-07 text-black fw-normal text-center mb-0 mb-md-2 mb-lg-3">
-          {currentData.para}
-          <span className="d-lg-block">{currentData.spanpara}</span>
-        </p>
-        <Row className="d-flex justify-content-center pt-lg-3">
-          {currentData.submap.map((levelsub, columnindex) => (
-            <Col
-              key={columnindex}
-              md={3}
-              xs={6}
-              className="p-sm-0 pt-3 pt-sm-3 pt-lg-0"
-            >
-              <div
-                className={`nextlevel-box ${columnindex === currentData.submap.length - 1
-                  ? "last-child"
-                  : ""
-                  }`}
+    <>
+      <Container>
+        <div className={` ${show ? 'pb-5' : ''}`}>
+          <p className="fs-xxl fw-semibold ff-fontspring text-center text-black mb-1">
+            {currentData.heading}
+          </p>
+          <p className={`fs-base ffBarlow opacity-07 text-black fw-normal text-center
+          ${show ? 'max-w-790 mx-auto mb-md-4' : ''}
+          `}>
+            {currentData.para}
+            <span className="d-lg-block">{currentData.spanpara}</span>
+          </p>
+          <Row className={`d-flex justify-content-center pt-lg-4`}>
+            {currentData.submap.map((levelsub, columnindex) => (
+              <Col
+                key={columnindex}
+                md={3}
+                xs={6}
+                className={`pt-3 pt-md-0 ${columnindex === currentData.submap.length - 1 ? 'last-child' : 'border-end-yellow'}`}
               >
-                <div className="d-flex justify-content-center justify-content-md-start">
-                  {levelsub.icon}
+                <div
+                  className={`
+                   ${show ? 'px-xl-4 mx-xl-2' : ''}
+                  `}
+                >
+                  <div className="d-flex justify-content-center justify-content-md-start">
+                    {levelsub.icon}
+                  </div>
+                  <p className={`ffBarlow fs-base fw-normal text-black text-center text-md-start
+                   ${show ? 'pt-2 mb-0' : ''}
+                  `}>
+                    {levelsub.paraBox}
+                  </p>
                 </div>
-                <p className="ffBarlow fs-base fw-normal text-black mb-0 pt-4 mt-2 mt-md-0 mt-lg-2 text-center text-md-start">
-                  {levelsub.paraBox}
-                </p>
-              </div>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    </Container>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </Container>
+    </>
   );
 };
 
